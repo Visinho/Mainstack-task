71bae4bb8d4b41b16da02c793c42d4ce1179e0dc
Visinho <Elvisvisinho@rocketmail.com>
2024-04-16 18:49:20 +0100

    Wrote the auth method using crypto

---
+++ src/helpers/index.ts
@@ -0,0 +1,9 @@
+import crypto from "crypto";
+import dotenv from 'dotenv';
+
+const SECRET = process.env.AUTHENTICATION_SECRET;
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac("sha256", [salt, password].join('/')).update(SECRET).digest("hex");
+}
