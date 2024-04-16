cbb151350875c8770a55c61a40b8f9d1d4891565
Visinho <Elvisvisinho@rocketmail.com>
2024-04-16 23:51:27 +0100

    Completed the creation of the login controller with sessiontoken

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -22,8 +22,41 @@ export const register = async (req: express.Request, res: express.Response) => {
                 salt, password: authentication(salt, password)
             }
         })
-        console.log(user)
         return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select("+authentication.salt + authentication.password");
+
+        if(!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password)
+
+        if(user.authentication.password !== expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        res.cookie("VISINHO-AUTH", user.authentication.sessionToken, { domain: "localhost", path: '/'});
+
+        return res.status(200).json(user).end();
+
+        await user.save();
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
